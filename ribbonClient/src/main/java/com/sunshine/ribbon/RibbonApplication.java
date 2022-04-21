650d1caefc46432f51abbcfa76dca10e4d5e3dd9
huzhanglin <799490931@qq.com>
2022-04-21 13:16:26 +0800

    1

--- ribbonClient/src/main/java/com/sunshine/ribbon/RibbonApplication.java
+++ ribbonClient/src/main/java/com/sunshine/ribbon/RibbonApplication.java
@@ -2,7 +2,9 @@ package com.sunshine.ribbon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(value = "com.sunshine")
 @SpringBootApplication
 public class RibbonApplication {
 
