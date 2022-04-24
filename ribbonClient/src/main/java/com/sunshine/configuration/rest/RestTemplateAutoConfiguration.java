ffd47956ae1b47e7526177b258fa92c5d6116c40
huzhanglin <799490931@qq.com>
2022-04-24 18:01:41 +0800

    ribbon

--- ribbonClient/src/main/java/com/sunshine/configuration/rest/RestTemplateAutoConfiguration.java
+++ ribbonClient/src/main/java/com/sunshine/configuration/rest/RestTemplateAutoConfiguration.java
@@ -1,7 +1,12 @@
 package com.sunshine.configuration.rest;
 
+import com.netflix.loadbalancer.IPing;
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.ResponseTimeWeightedRule;
+import com.netflix.loadbalancer.Server;
+import com.sunshine.util.RestTemplateUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +15,8 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateAutoConfiguration {
+
+    private final static Logger logger = LoggerFactory.getLogger(RestTemplateAutoConfiguration.class);
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
@@ -26,6 +33,19 @@ public class RestTemplateAutoConfiguration {
     @Bean
     public IRule iRule(){
         return new ResponseTimeWeightedRule();
+    }
+
+    @Bean
+    public MyPing iPing(){
+        return new MyPing();
+    }
+
+    static public class MyPing implements IPing{
+        @Override
+        public boolean isAlive(Server server) {
+            logger.info(" ping ...{}:{}",server.getHost(),server.getPort());
+            return true;
+        }
     }
 
 }
