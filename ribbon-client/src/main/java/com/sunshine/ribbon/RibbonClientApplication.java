374da59a6634dad1b77e5806f57e97b53f56350f
huzhanglin <799490931@qq.com>
2022-04-25 09:35:39 +0800

    工程搭建

--- ribbon-client/src/main/java/com/sunshine/ribbon/RibbonClientApplication.java
+++ ribbon-client/src/main/java/com/sunshine/ribbon/RibbonClientApplication.java
@@ -2,14 +2,15 @@ package com.sunshine.ribbon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
-
+@EnableEurekaClient
 @ComponentScan(value = "com.sunshine")
 @SpringBootApplication
-public class RibbonApplication {
+public class RibbonClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RibbonApplication.class, args);
+        SpringApplication.run(RibbonClientApplication.class, args);
     }
 
 }
