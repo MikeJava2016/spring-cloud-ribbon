6f6aeff8f6dcce8615c736e059d21a863088deae
huzhanglin <799490931@qq.com>
2022-04-27 09:18:16 +0800

    gateway

---
+++ ribbon-gateway/src/main/java/com/sunshine/RibbonGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.sunshine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//https://gitee.com/mirrors/fizz-gateway?_from=gitee_search
+@SpringBootApplication
+public class RibbonGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RibbonGatewayApplication.class, args);
+    }
+
+}
---
+++ ribbon-gateway/src/test/java/com/sunshine/RibbonGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunshine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RibbonGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
