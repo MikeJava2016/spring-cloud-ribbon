bd120445f8802a76e3f828e046f35ca7d32dba03
huzhanglin <799490931@qq.com>
2022-05-22 22:31:44 +0800

    整合sharding jdbc 单元测试成功

--- ribbon-security/src/main/java/com/sunshine/security/GpVipSecurity1011Application.java
+++ ribbon-security/src/main/java/com/sunshine/security/GpVipSecurity1011Application.java
@@ -1,8 +1,9 @@
 package com.sunshine.security;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan(basePackages = "com.sunshine.security.mapper")
 @SpringBootApplication(scanBasePackages = "com.sunshine")
 public class GpVipSecurity1011Application {
 
