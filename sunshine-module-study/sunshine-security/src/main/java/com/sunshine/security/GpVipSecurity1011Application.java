2d888d12657a42734d940085eaa2b60489db1591
huzhanglin <799490931@qq.com>
2022-05-24 09:47:50 +0800

    项目结构调整

--- sunshine-module-study/sunshine-security/src/main/java/com/sunshine/security/GpVipSecurity1011Application.java
+++ sunshine-module-study/sunshine-security/src/main/java/com/sunshine/security/GpVipSecurity1011Application.java
@@ -3,6 +3,7 @@ package com.sunshine.security;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @MapperScan(basePackages = "com.sunshine.security.mapper")
 @SpringBootApplication(scanBasePackages = "com.sunshine")
 public class GpVipSecurity1011Application {
