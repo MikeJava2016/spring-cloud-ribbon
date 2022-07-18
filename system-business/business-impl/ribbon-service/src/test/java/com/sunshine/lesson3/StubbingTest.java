5578c3d3a05166e3ef26261f2816e4a3f2f2b4fc
799490931@qq.com <799490931@qq.com>
2022-07-18 11:01:58 +0800

    stubbing

--- system-business/business-impl/ribbon-service/src/test/java/com/sunshine/lesson3/StubbingTest.java
+++ system-business/business-impl/ribbon-service/src/test/java/com/sunshine/lesson3/StubbingTest.java
@@ -5,6 +5,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +19,7 @@ import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
 
+
 /**
  * @version v1
  * @Description TODO
@@ -25,13 +29,43 @@ import static org.mockito.Mockito.*;
 @RunWith(MockitoJUnitRunner.class)
 public class StubbingTest {
 
-
+    private final static Logger logger = LoggerFactory.getLogger(StubbingTest.class);
     private List<String> list;
 
     @Before
     public void  init() {
         this.list = mock(ArrayList.class);
     }
+
+    @Test
+    public void stubbingWithRealCall(){
+        StubbingService stubbingService = mock(StubbingService.class); // cglib生成代理类
+        logger.info("stubbingService = {}",stubbingService.getClass());
+
+        System.out.println( stubbingService.getS("10"));
+        // 调用mock方法
+        doReturn("102").when(stubbingService).getS(anyString());
+        assertThat(stubbingService.getS(anyString()),equalTo("102"));
+        // 调用真实的方法
+        when(stubbingService.getI(anyInt())).thenCallRealMethod();
+        assertThat(stubbingService.getI(10),equalTo(100));
+
+
+    }
+
+    /**
+     * 定义返回值逻辑
+     */
+    @Test
+    public void stubbingWithAnswer(){
+        when(list.get(anyInt())).thenAnswer((Answer<String>) invocation -> {
+            Integer argument = invocation.getArgument(0, Integer.class);
+            return String.valueOf(argument *10);
+        });
+        assertThat(list.get(0),equalTo("0"));
+        assertThat(list.get(99),equalTo("990"));
+    }
+
 
     @Test
     public void howToUserStubbing(){
@@ -75,8 +109,28 @@ public class StubbingTest {
         assertThat("add 1",list.add("2"));
 //        assertThat(list.add(anyString()),equalTo(true));
 
+        when(list.get(0)).thenReturn("1");
+        doReturn("2").when(list.get(1));
+
+        assertThat(list.get(0),equalTo("1"));
     }
 
+
+
+    @Test
+    public void iteratorSubbing(){
+        when(list.size()).thenReturn(1, 2, 3, 4);
+        assertThat(list.size(),equalTo(1));
+        assertThat(list.size(),equalTo(2));
+        assertThat(list.size(),equalTo(3));
+        assertThat(list.size(),equalTo(4));
+        assertThat(list.size(),equalTo(4));
+        when(list.size()).thenReturn(10).thenReturn(11).thenReturn(12);
+        assertThat(list.size(),equalTo(10));
+        assertThat(list.size(),equalTo(11));
+        assertThat(list.size(),equalTo(12));
+        assertThat(list.size(),equalTo(12));
+    }
 
     @After
     public void destory() {
