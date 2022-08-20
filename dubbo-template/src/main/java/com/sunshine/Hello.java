8c24c8faba358d2fa2bbeab0a52666b133c1b843
799490931@qq.com <799490931@qq.com>
2022-08-20 19:53:01 +0800

    hotfix

---
+++ dubbo-template/src/main/java/com/sunshine/Hello.java
@@ -0,0 +1,13 @@
+package com.sunshine;
+
+/**
+ * @version v1
+ * @Description TODO
+ * @Author huzhanglin
+ * @Date 2022/8/20 下午 07:51
+ **/
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println(1233);
+    }
+}
